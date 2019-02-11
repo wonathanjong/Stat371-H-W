d5db0dd8a61d486cf616a800d145cd37345c604d
wongmac <hellogug@yahoo.com>
2019-02-10 23:24:41 -0600

    added classnotes for carol

--- ClassNotes_2-4-19.R
+++ ClassNotes_2-4-19.R
@@ -1,3 +1,4 @@
+library(mosaic)
 #ford ev
 mf = 0.12
 #ford sd
@@ -8,3 +9,27 @@ mt = 0.14
 st = 3.12
 #covariance of both variables
 cc = 3.063
+
+#weight of portfolio
+w = seq(0,1,by=0.1)
+#mean portfolio
+mp = w*mf + (1-w)*mt
+#variance
+vp = w^2*sf^2 + (1-w)^2*st^2 + 2*w*(1-w)*cc
+#standard deviation
+sp = sqrt(vp)
+sharpe = mp/sp
+
+plot(w, sharpe)
+
+value0 = 100
+value = do(num.sim) * {
+  returns = rnorm(20,mean = 6, sigma = 15)
+  compounding.factors = (1+returns/100)
+  value20 = value0*prod(compounding.factors)
+}
+
+hist(value$results)
+mean(value$results)
+median(value$results)
+plot(density(value$result))
